6860f6d0095082208d72ab1a11012db7b812e6ea
broVeider <97959375+broVeider@users.noreply.github.com>
2024-04-16 15:26:34 +0300

    Update smart_monitor.sh
    Fixed error for machines without HW raid
    Fixed output for several metrics

--- scripts/smart_monitor.sh
+++ scripts/smart_monitor.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+# Set smartctl binary
 SMARTCTL=/usr/sbin/smartctl
+
+# Path to device list
 SMARTSCAN=/opt/smartsscan
-#DISKS=(`lsblk 2>&1| grep -E  "^nvm|^sd" | grep -v NAME | grep -v $HWRAID| awk '{print $1}'`)
+
+# Path to node_exporter textfile colletor directory
 DIR=/var/lib/node_exporter_textfile_collectors
+
+# Temp dir for smartctl output
 TMPDIR=/opt/smarts
+[ -d $TMPDIR ] || mkdir $TMPDIR
+
+# Temp dir for hardware raid devices 
 TMPHW=/opt/hwraid
 [ -d $TMPHW ] || mkdir $TMPHW
+
+# Get device list 
+$SMARTCTL --scan >$SMARTSCAN
+
+# Get hardware raid devices (currently PERC)
+HWRAID=`cat $SMARTSCAN| grep megaraid -B1 | grep -v megaraid | awk '{print $1}'| cut -d\/ -f3`
 
 function collect_smart() {
 	disks=("$@")
@@ -38,8 +53,6 @@ function collect_hwraid() {
                     else sata_status
                     fi
         done
-#       rm -f /opt/smarts/*
-#       rm -f $SMARTSCAN
 }
 
 function sata_status () {
@@ -48,11 +61,11 @@ function sata_status () {
                        then echo "${disk}${num}_failed_state 1" > $outfile
                        else echo "${disk}${num}_failed_state 0" > $outfile
                   fi
-		  temp=`echo \"$tmpstatus\" | grep "Temperature_Celsius" | awk '{print $10}'`
+		  temp=`echo "$tmpstatus" | grep "Temperature_Celsius" | awk '{print $10}'`
                   [[ -n $temp ]] && echo "${disk}${num}_temperature $temp" >> $outfile
-		  read_errors=`echo \"$tmpstatus\" | grep "Raw_Read_Error_Rate" | awk '{print $10}'`
+		  read_errors=`echo "$tmpstatus" | grep "Raw_Read_Error_Rate" | awk '{print $10}'`
                   [[ -n $read_errors ]] && echo "${disk}${num}_read_errors $read_errors" >> $outfile
-		  write_errs=`echo \"$tmpstatus\" | grep "Write_Error_Rate" | awk '{print $10}'`
+		  write_errs=`echo "$tmpstatus" | grep "Write_Error_Rate" | awk '{print $10}'`
 		  [[ -n "$write_errs" ]] && echo "${disk}${num}_write_errors $write_errs" >> $outfile
 		  lifetime=`echo "$tmpstatus" | grep "Percent_Lifetime_Remain" | awk '{print $10}'`
 		  [[ -z "$lifetime" ]] &&  lifetime=`echo "$tmpstatus" | grep "Remaining_Lifetime_Perc" | awk '{print $10}'`
@@ -67,21 +80,22 @@ function nvme_status () {
                        then echo "${disk}${num}_failed_state 1" > $outfile
                        else echo "${disk}${num}_failed_state 0" > $outfile
                   fi
-		  temp=`echo \"$tmpstatus\" | grep "^Temperature:" | awk '{print $2}'`
+		  temp=`echo "$tmpstatus" | grep "^Temperature:" | awk '{print $2}'`
                   [[ -n $temp ]] && echo "${disk}${num}_temperature $temp" >> $outfile
-		  smart_errors=`echo \"$tmpstatus\" | grep "^Error Information Log Entries:" | awk '{print $5}'|cut -d\% -f1`
+		  smart_errors=`echo "$tmpstatus" | grep "^Error Information Log Entries:" | awk '{print $5}'|cut -d\% -f1`
 		  [[ -n $smart_errors ]] && echo "${disk}${num}_smart_errors $smart_errors" >> $outfile
-		  lifetime=`echo \"$tmpstatus\" | grep "^Percentage Used:" | awk '{print $3}'|cut -d\% -f1`
+		  lifetime=`echo "$tmpstatus" | grep "^Percentage Used:" | awk '{print $3}'|cut -d\% -f1`
                   [[ -n $lifetime ]] && echo "${disk}${num}_perc_used $lifetime" >> $outfile
                   chown node_exporter.node_exporter $outfile
 	}
 
+# Collect devices and get info for hardware raid
+if [ -n "$HWRAID" ]
+then 	HWRAIDDEVS=(`cat $SMARTSCAN |grep megaraid | cut -d\, -f2 | cut -d\  -f 1`)
+       	DISKS=(`lsblk 2>&1| grep -E  "^nvm|^sd" | grep -v NAME | grep -v $HWRAID | awk '{print $1}'`)
+	collect_hwraid ${HWRAIDDEVS[*]}
+else DISKS=(`lsblk 2>&1| grep -E  "^nvm|^sd" | grep -v NAME | awk '{print $1}'`)
+fi
 
-$SMARTCTL --scan >$SMARTSCAN
-HWRAID=`cat $SMARTSCAN| grep megaraid -B1 | grep -v megaraid | awk '{print $1}'| cut -d\/ -f3`
-HWRAIDDEVS=(`cat $SMARTSCAN |grep megaraid | cut -d\, -f2 | cut -d\  -f 1`)
-#DISKS=(`lsblk 2>&1| grep -E  "^nvm|^sd" | grep -v NAME | grep -v $HWRAID| awk '{print $1}'`)
-DISKS=(`lsblk 2>&1| grep -E  "^nvm|^sd" | grep -v NAME | grep -v $HWRAID | awk '{print $1}'`)
+# Collect smart for other devices
 collect_smart ${DISKS[*]}
-collect_hwraid ${HWRAIDDEVS[*]}
-#for run in {1..11} ; do collect_smart ${DISKS[*]}; sleep 5 ; done
